0ee0bcf11ff979cfe7e34b2e8fe268ab0130d204
mfrisch87 <mikey.frisch@gmail.com>
2021-03-09 16:56:39 -0800

    added azure cognitive search, working on getting working

--- social_app/src/App.js
+++ social_app/src/App.js
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import GoogleMap from './components/GoogleMap';
+import main from './components/bingNews';
 function App() {
 
 // const [] = useState()
---
+++ social_app/src/components/bingNews.js
@@ -0,0 +1,28 @@
+const { NewsSearchClient } = require("@azure/cognitiveservices-newssearch");
+const { CognitiveServicesCredentials } = require("@azure/ms-rest-azure-js");
+ 
+async function main() {
+  const newsSearchKey = process.env["newsSearchKey"] || "<newsSearchKey>";
+  const cognitiveServiceCredentials = new CognitiveServicesCredentials(
+    newsSearchKey
+  );
+  const client = new NewsSearchClient(cognitiveServiceCredentials);
+  const query = "Microsoft Azure";
+  const options = {
+    count: 10,
+    freshness: "Month",
+    safeSearch: "Strict"
+  };
+  client.news
+    .search(query, options)
+    .then(result => {
+      console.log("The result is:");
+      console.log(result);
+    })
+    .catch(err => {
+      console.log("An error occurred:");
+      console.error(err);
+    });
+}
+ 
+main();
