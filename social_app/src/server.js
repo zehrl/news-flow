f84db960f7fdf685f1aeff3bbb3aad277d792588
CoryKDev <corykeelan7@gmail.com>
2021-03-10 21:10:29 -0800

    basic express app

---
+++ social_app/src/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const morgan = require('morgan');
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
