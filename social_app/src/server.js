6dc53fc4efa38ed4867c6d7adc7056facd61438c
CoryKDev <corykeelan7@gmail.com>
2021-03-11 01:28:19 -0800

    deleted unneeded code

--- social_app/src/server.js
+++ social_app/src/server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
-const app = express();
 
+const app = express();
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
