d8adafea9804c26258a357c4e1188b12fccf9871
Logan Zehr <zehrl315@outlook.com>
2021-03-21 20:15:30 -0700

    Date is now formatted correctly on news cards.

--- client/src/components/NewsCard.js
+++ client/src/components/NewsCard.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const NewsCard = ({ article: { url, title, description, publishedDate, thumbnail, category, provider } }) => {
 // const NewsCard = () => { 
-
   return (
 
     <a href={url} target="_blank" rel="noopener noreferrer" className="card mb-3 news-card" style={{maxWidth: "600px", margin: "auto"}}>
@@ -11,8 +10,8 @@ const NewsCard = ({ article: { url, title, description, publishedDate, thumbnail
           src={thumbnail} alt="...">
         </img>
         <div className="d-flex flex-column flex-grow-1 align-items-stretch">
-          <div className="d-flex justify-content-between align-items-start">
-            <h5 className="card-title article-title me-2">{title}</h5>
+          <div className="d-flex justify-content-between align-items-start mb-2">
+            <h5 className="card-title article-title mb-0 me-2">{title}</h5>
             <button className="btn btn-primary save-button" type="submit">Save</button>
           </div>
           <p className="card-text article-description mb-1">{description}</p>
