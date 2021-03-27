65513c881f07ea05b0bc4a8cdef977b47f2e990e
mfrisch87 <mikey.frisch@gmail.com>
2021-03-27 10:12:39 -0700

    sorting out save and delete functions

--- client/src/components/NewsCard.js
+++ client/src/components/NewsCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import favoriteAPI from '../utils/favoriteAPI';
 
 const NewsCard = ({ article: { url, title, description, publishedDate, thumbnail, category, provider } }) => {
