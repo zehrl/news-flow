fd98ff97f6717a60ba379b287dcbea0d14efec21
mfrisch87 <mikey.frisch@gmail.com>
2021-03-26 17:00:16 -0700

    started API routes for saving/deleting favorites

---
+++ client/src/utils/favoriteAPI.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+export default {
+    getFavorites: function(email) {
+        return axios.get("/api/favorites", + email);
+    },
+    saveArticle: function(articleData) {
+        return axios.post("/api/favorites", articleData)
+    },
+    deleteFavorite: function(id) {
+        return axios.delete("/api/favorites", + id)
+    }
+};
