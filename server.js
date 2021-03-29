f659b65040fb1ba87a62611ce7143715e90ab3ab
Logan Zehr <zehrl315@outlook.com>
2021-03-29 15:37:23 -0700

    Added app get /* path to help fix server issues on Heroku.

--- server.js
+++ server.js
@@ -30,9 +30,14 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/public/index.html"));
+// });
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 
 app.listen(PORT, function (error) {
   if (error) { throw error } else { console.log(`🌎 ==> API server now on port ${PORT}!`) };
