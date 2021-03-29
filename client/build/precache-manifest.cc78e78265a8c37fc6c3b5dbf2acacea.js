514ac46feac6ab489f5d123fb46c82aae6f298b9
Logan Zehr <zehrl315@outlook.com>
2021-03-29 15:30:42 -0700

    Built production static folder

---
+++ client/build/precache-manifest.cc78e78265a8c37fc6c3b5dbf2acacea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "066e07251c256a47919e",
+    "url": "/static/css/main.873ced23.chunk.css"
+  },
+  {
+    "revision": "066e07251c256a47919e",
+    "url": "/static/js/main.debd9494.chunk.js"
+  },
+  {
+    "revision": "7cc30f45478ca46f1ef4",
+    "url": "/static/js/runtime~main.81aa0dc5.js"
+  },
+  {
+    "revision": "1b8adeca0d45ab95cccb",
+    "url": "/static/css/2.1e97d7f2.chunk.css"
+  },
+  {
+    "revision": "1b8adeca0d45ab95cccb",
+    "url": "/static/js/2.637e1635.chunk.js"
+  },
+  {
+    "revision": "c32727ffd368de57818a",
+    "url": "/static/js/3.30f51477.chunk.js"
+  },
+  {
+    "revision": "7e7de3c41198538567282c69a5dc4cf2",
+    "url": "/static/media/logo.7e7de3c4.png"
+  },
+  {
+    "revision": "2f3ac40a56b721b63967c413c4592eab",
+    "url": "/static/media/darkmode.2f3ac40a.png"
+  },
+  {
+    "revision": "51fef0ce83efe5145277f55fbef1f513",
+    "url": "/index.html"
+  }
+];
